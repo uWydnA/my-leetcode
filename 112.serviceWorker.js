0d391c2a046e9a9858f26ef70e6e4998f10fdec8
retr0 <retr0@retr0deMacBook-Pro.local>
2020-10-20 17:57:52 +0800

    chore: serviceWorker

--- 112.serviceWorker.js
+++ 112.serviceWorker.js
@@ -24,9 +24,7 @@ self.addEventListener('install', e => {
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(function(response) {
-      if (response) {
-        return response
-      }
+      if (response) return response
       console.log('fetch  source')
     })
   )
